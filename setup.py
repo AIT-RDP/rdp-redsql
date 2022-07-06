9533df24f9ebc65359d774e6cf03e0a747c24951
Michael Spiegel <michael.spiegel@ait.ac.at>
2022-07-06 10:30:40 +0200

    feat: Initial setup scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+import versioneer
+from setuptools import find_packages, setup
+
+setup(
+    name="redsql",
+    packages=find_packages(exclude=["test", "test.*"]),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Generic Redis to SQL data synchronizer.",
+    author="AIT Austrian Institute of Technology",
+    license="Proprietary",
+    url="https://gitlab-intern.ait.ac.at/ees/rdp/generic-components/redsql",
+    setup_requires=["pytest-runner"],
+    install_requires=["redis", "hiredis", "python-dotenv", "sqlalchemy", "psycopg2-binary"],
+    test_requires=["pytest"],
+)
