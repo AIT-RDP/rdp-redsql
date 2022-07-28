4fbb5bbc99ffbb59ea397aa79857d0f74bb4cdc5
Michael Spiegel <michael.spiegel@ait.ac.at>
2022-07-28 16:38:52 +0200

    fix: Error on reading from empty streams (#6)

--- redsql/channel.py
+++ redsql/channel.py
@@ -61,8 +61,9 @@ class _RedisStreamSource:
 
         message = self._redis_client.xreadgroup(self._group_name, self._consumer_name, {self._stream_name: ">"},
                                                 count=1, block=2000)
-        assert len(message) <= 1, "At most one message expected"
-        if len(message) >= 1:
+        assert len(message) == 1, "Exactly one stream expected"
+        assert len(message[0][1]) <= 1, "At most one message expected"
+        if len(message[0][1]) >= 1:
             assert message[0][0] == self._stream_name, "Received message from invalid stream"
             self._last_message_id = message[0][1][0][0]
             message_content = message[0][1][0][1]
