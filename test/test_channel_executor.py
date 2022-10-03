c598ba005100f5585d9ca4c3b09e8e284b5e6ef4
Michael Spiegel <michael.spiegel@ait.ac.at>
2022-10-03 17:10:33 +0200

    feat: Bulk insert and query test-case (#29)

--- test/test_channel_executor.py
+++ test/test_channel_executor.py
@@ -9,6 +9,7 @@ import sqlalchemy as sql
 
 import redsql.channel as channel
 import redsql.channel_executor as channel_executor
+import test_channel
 
 
 class MockupChannel(channel.Channel):
@@ -76,6 +77,58 @@ def test_thread_executor_channel_calls(redis_pool, sql_engine):
     assert 1 <= mockup_channel.exec_invocations <= 3
     assert mockup_channel.open_invocations == 1
     assert mockup_channel.close_invocations == 1
+
+
+def test_bulk_channel_operation(redis_pool, sql_engine, reference_table, test_table):
+    """Tests the end-to-end operation using several concurrent channels"""
+
+    redis_client = redis.Redis(connection_pool=redis_pool)
+
+    config = {}
+    for i in range(100):
+
+        # Assemble the individual channel configuration
+        config[f"Chn.{i}"] = {
+            "trigger": {"stream id": f"test-stream.{i}"},
+            "encoding": {
+                "_default": "JSON",
+                "observation_time": "JSONDatetimeString"
+            },
+            "steps": [
+                {
+                    "type": "CachedSQLQuery",
+                    "cache keys": ["dp_id"],
+                    "query": f"SELECT value_text FROM {reference_table} WHERE dp_id=:dp_id"
+                }
+            ],
+            "data sink": {
+                "table": test_table,
+                "columns": {
+                    "dp_id": "dp_id",
+                    "value_text": "value_text",
+                    "value_float": "value_float",
+                    "value_int": "value_int"
+                }
+            }
+        }
+
+        # Add some test messages
+        for j in range(100, 200):
+            redis_client.xadd(f"test-stream.{i}", fields={
+                "dp_id": f"{j}",
+                "value_float": f"{j * 100 + i}",
+                "value_int": f"{i}"
+            })
+
+    supervisor = channel_executor.ChannelSupervisor(config, redis_pool, sql)
+    supervisor.start()
+
+    while test_channel.read_test_table(sql_engine, test_table).index.size != 100*100:
+        time.sleep(1)
+        status = supervisor.heartbeat()
+        assert status == {f"Chn.{i}": "ok" for i in range(100)}
+
+    supervisor.stop()
 
 
 class ErrMockupChannel(channel.Channel):
