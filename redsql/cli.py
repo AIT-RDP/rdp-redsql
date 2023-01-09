f678371122ac56c1525fb44c264b9aa156e71f6f
VettorettiD <denis.vettoretti@ait.ac.at>
2023-01-09 11:51:19 +0100

    Add support for Redis password REDSQL config

--- redsql/cli.py
+++ redsql/cli.py
@@ -175,8 +175,9 @@ def _load_redis_connection_pool(config: dict) -> redis.ConnectionPool:
     db = redis_config["db"]
     logger.debug(f"Configure redis connection to {host}:{port} using db {db}")
 
-    if os.getenv('REDSQL_REDIS_PASSWORD') is not None:
-        pool = redis.ConnectionPool(host=host, port=port, db=db, password=os.getenv('REDSQL_REDIS_PASSWORD'),
+    if 'password' in redis_config:
+        pool = redis.ConnectionPool(host=host, port=port, db=db,
+                                    password=redis_config['password'],
                                     decode_responses=True)
     else:
         pool = redis.ConnectionPool(host=host, port=port, db=db, decode_responses=True)
