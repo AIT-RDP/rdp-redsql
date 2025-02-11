b7afdebbd60e1a2d749dea8a1c0bcccbc7643e09
Michael Spiegel <michael.spiegel@ait.ac.at>
2025-02-11 15:52:12 +0100

    feat: Introduce ResolveDataType step (#48)

--- redsql/steps/__init__.py
+++ redsql/steps/__init__.py
@@ -2,3 +2,4 @@ from .packing import PackMessageValues
 from .partitioning import SplitByKey
 from .partitioning import UnpackArrayValues
 from .sql import CachedSQLQuery
+from .transformation import ResolveDataType
